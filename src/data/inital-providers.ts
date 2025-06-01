3a689e78b907d3196a71fb8779d1543bd3705e4f
kweng <kwengerkw@gmail.com>
2025-06-01 19:06:30 -0400

    Add Initial Provider Type and Test Data
    Initial provider type:
     - Id: id of provider will be unique for use of
      editing providers if I add that capability
      - First/Last/Provider Name: Name of the provider
      - Email: provider email
        - First/Last/Email are all required fields
        - I made provider name required also, but it will
          be created from first/last name
        - Id will also be code generated so will not
          appear to the user when adding a provider
      - Optional values per overview:
        -practice name and speciality

---
+++ src/data/inital-providers.ts
@@ -0,0 +1,67 @@
+import type { Provider } from "@/types/provider.ts"
+
+export const initialProviders: Provider[] = [
+    {
+        id: "1",
+        firstName: "Mike",
+        lastName: "Harris",
+        email: "mharris@updox.com",
+        practiceName: "Harris Pediatrics",
+        specialty: "Pediatrics",
+        providerName: "Harris, Mike"
+    },
+    {
+        id: "2",
+        firstName: "Adam",
+        lastName: "Partyka",
+        email: "apartyka@updox.com",
+        practiceName: "Partyka Podiatry",
+        specialty: "Podiatry",
+        providerName: "Partyka, Adam"
+    },
+    {
+        id: "3",
+        firstName: "Nate",
+        lastName: "Rose",
+        email: "nrose@updox.com",
+        practiceName: "Carlson Orthopedics",
+        specialty: "Orthopedics",
+        providerName: "Rose, Nate"
+    },
+    {
+        id: "4",
+        firstName: "Mike",
+        lastName: "Witting",
+        email: "mwitting@updox.com",
+        practiceName: "Witting's Well Kids Pediatrics",
+        specialty: "Pediatrics",
+        providerName: "Witting, Mike"
+    },
+    {
+        id: "5",
+        firstName: "Tobin",
+        lastName: "Juday",
+        email: "tjuday@updox.com",
+        practiceName: "Juday Family Practice",
+        specialty: "General Medicine",
+        providerName: "Juday, Tobin"
+    },
+    {
+        id: "6",
+        firstName: "Jess",
+        lastName: "Miller",
+        email: "jmiller@updox.com",
+        practiceName: "Miller's Family Clinic",
+        specialty: "Psychiatry",
+        providerName: "Miller, Jess"
+    },
+    {
+        id: "7",
+        firstName: "Robert",
+        lastName: "Davis",
+        email: "rdavis@updox.com",
+        practiceName: "Advanced Medical Associates",
+        specialty: "Endocrinology",
+        providerName: "Davis, Robert"
+    },
+]
---
+++ src/types/provider.ts
@@ -0,0 +1,9 @@
+export interface Provider {
+    id: string
+    firstName: string
+    lastName: string
+    email: string
+    practiceName?: string
+    specialty?: string
+    providerName: string
+}
