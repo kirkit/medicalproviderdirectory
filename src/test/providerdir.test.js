aa8767b7c61822171b4a656a3040566d41dbbae3
kweng <kwengerkw@gmail.com>
2025-06-01 19:39:07 -0400

    Rebase off latest changes

---
+++ src/test/providerdir.test.js
@@ -0,0 +1,20 @@
+//Tests for adding to provider directory
+    //Need to test for duplicate providers
+    //Need to test for invalid providers
+    //Need to test for empty First/Last/Email
+    //Need to be sure speciality and practice name are optional
+// Tests for removing from provider directory
+    //Need to be sure only desired provider is removed
+    //Need to decicde if needed a confirmation dialog for removal, and if so needs to be tested
+// Tests for searching for providers
+    //Test for searching by speciality
+    //Test for searching by practice name
+    //Test for searching by email
+    //Test for searching by name
+        //Also will need to test for when last name are the same but other values are different
+// Tests for sorting providers/
+    //Need to test for sorting by name
+        //Also need to test for when last name are the same but first name are different, they are sorted correctly
+    //Need to test for sorting by speciality
+    //Need to test for sorting by practice name
+    //Need to test for sorting by email
